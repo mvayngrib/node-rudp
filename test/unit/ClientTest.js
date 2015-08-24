70cee0ed8bee92a405693669859bc3bf3fce118f
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-08-24 10:28:32 +0100

    simplify send() callback, add test

--- test/unit/ClientTest.js
+++ test/unit/ClientTest.js
@@ -1,4 +1,5 @@
 
+var expect = require('expect.js');
 var dgram = require('dgram')
 var Client = require('../../lib/Client');
 
@@ -23,6 +24,33 @@ describe('open/close', function () {
             done()
           })
         })
+      })
+    })
+  })
+
+  it('calls back on successful msg delivery', function(done) {
+    this.timeout(10000)
+    var s1 = dgram.createSocket('udp4')
+    var s2 = dgram.createSocket('udp4')
+    s1.bind(function() {
+      s2.bind(function() {
+        var bill
+        var ted = new Client(s2, '127.0.0.1', s1.address().port)
+        var received
+        ted.send(new Buffer('excellent!'), function () {
+          expect(received).to.eql(true)
+          bill.close()
+          ted.close()
+          done()
+        })
+
+        setTimeout(function () {
+          // wait to pick up
+          bill = new Client(s1, '127.0.0.1', s2.address().port)
+          bill.on('data', function(data) {
+            received = true
+          })
+        }, 1000)
       })
     })
   })
